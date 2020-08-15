4b9a1e789aedac7ab97078c24b753e56a6e82022
Jacob Wheat <jacob.wheat@c02vp6cehtd6.corp.instructure.com>
2020-08-14 23:27:14 -0600

    made findAll, by Id and save functions for the db.

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -13,3 +13,23 @@ module.exports = {
             .catch(err => res.status(422).json(err))
 
     },
+      // find all books saved in db
+      findAll: (req, res) => {
+        db.Book
+            .find(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    findById: (req, res) => {
+        db.Book
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    // used to save a book to the db
+    save: (req, res) => {
+        db.Book
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
