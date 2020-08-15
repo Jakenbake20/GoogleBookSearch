8fb4595605b5b379d3a59ae9d3397b9a2f53a08a
Jacob Wheat <jacob.wheat@c02vp6cehtd6.corp.instructure.com>
2020-08-14 23:47:57 -0600

    finished list

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -30,3 +30,64 @@ export function BookListItem(props) {
                 err => console.log(err)
             )
     };
+
+    // function to handle deleting book from db when delete button is clicked
+    const handleDeleteBtn = event => {
+        API.deleteBook(props.id)
+            .then(
+                res => {
+                    // use loadBooks prop from Saved page component
+                    props.loadBooks()
+                    console.log(props.id)
+                }
+            )
+            .catch(err => console.log(err))
+    };
+
+    return (
+        <li className="list-group-item" key={props.id}>
+            <Container>
+                <Row>
+                    <Col size="xs-4 sm-2">
+                        <Thumbnail src={props.image} />
+                    </Col>
+                    <Col size="xs-8 sm-10">
+                        <h3>{props.title}</h3>
+                        <p>
+                            Written By {[props.authors].flat().join(", ")}
+                        </p>
+                        <p>
+                            {props.description}
+                        </p>
+                        <a
+                            rel="noreferrer noopener"
+                            className="btn btn-lg btn-primary input-lg view"
+                            target="_blank"
+                            href={props.link}
+                        >
+                            View
+                        </a>
+                        {/* if there is an object id render the SaveBtn component else render the DeleteBtn component */}
+                        {!props.id ?
+                            <SaveBtn
+                                type="success"
+                                className="input-lg"
+                                onClick={handleSaveBtn}
+                            >
+                                Save
+                            </SaveBtn>
+                            :
+                            <DeleteBtn
+                                type="danger"
+                                className="input-lg"
+                                onClick={handleDeleteBtn}
+                            >
+                                Delete
+                            </DeleteBtn>
+                        }
+                    </Col>
+                </Row>
+            </Container>
+        </li>
+    );
+};
