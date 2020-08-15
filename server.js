e810891b0d86bddc0063365c8fe585a8b9ba7d60
Jacob Wheat <jacob.wheat@c02vp6cehtd6.corp.instructure.com>
2020-08-14 23:18:00 -0600

    started middleware.

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const apiRoutes = require("./routes/index");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
